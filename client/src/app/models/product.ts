3737fc82b8321fe9f0463ffd73ce3a2f7a8f1b0d
Songyang <songyangtian44@gmail.com>
2023-04-21 15:31:34 +1200

    End of Section 8

---
+++ client/src/app/models/product.ts
@@ -0,0 +1,11 @@
+
+  export interface Product {
+    id: number
+    name: string
+    description: string
+    pricce: number
+    pictureUrl: string
+    productType: string
+    productBrand: string
+  }
+  
