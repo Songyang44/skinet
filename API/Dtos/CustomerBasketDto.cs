a132c57541476705273511f37c3eacc9df5946f0
Songyang <songyangtian44@gmail.com>
2023-05-14 15:45:41 +1200

    End of Section 16

---
+++ API/Dtos/CustomerBasketDto.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Core.Entity;
+using System.ComponentModel.DataAnnotations;
+
+namespace API.Dtos
+{
+    public class CustomerBasketDto
+    {
+        [Required]
+        public string Id { get; set; }
+        public List<BasketItemDto> Items { get; set; }
+    }
+}
